5440df34b644d9fdd2431c2f648bff4f2726e66f
ashishrock9394 <ashishkumar9394@gmail.com>
2025-02-18 13:32:34 +0530

    alert

--- resources/views/user/layouts/header.blade.php
+++ resources/views/user/layouts/header.blade.php
@@ -76,30 +76,21 @@
                </nav>
             </div>
          </header>
-         @if(session('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{ session('success') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-@endif
-
-@if(session('error'))
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        {{ session('error') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-@endif
+         @if (session('success'))
+               <div id="success-message" class="alert alert-success">
+               {{ session('success') }}
+               </div>
+         @endif
+         @if (session('error'))
+               <div id="error-message" class="alert alert-danger">
+               {{ session('error') }}
+               </div>
+         @endif
 
       <script>
-         $(document).ready(function(){
-            setTimeout(function() {
-                  $(".alert").fadeOut("slow");
-            }, 3000); // Hides after 3 seconds
-         });
+         setTimeout(function() {
+               $(".alert").fadeOut("slow");
+         }, 3000);
       </script>
 
 
